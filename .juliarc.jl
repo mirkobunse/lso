5c23e606f582ac5fb496d517ed626386d47c87a0
Mirko Bunse <mirko.bunse@tu-dortmund.de>
2016-05-26 19:58:09 +0200

    Dynamic inclusion of files in src and tst folder
    Signed-off-by: Mirko Bunse <mirko.bunse@tu-dortmund.de>

--- .juliarc.jl
+++ .juliarc.jl
@@ -1,3 +1,12 @@
-include("Opt.jl")
-include("Obj.jl")
-include("Tst.jl")
+# includes all julia files from a given directory
+function includefiles(dir::AbstractString)
+	for filename in readdir(dir)
+		if endswith(filename, ".jl")
+			include(dir*"/"*filename) # concat
+			println("Included ", dir*"/"*filename)
+		end
+	end
+end
+
+includefiles("src")
+includefiles("tst")
