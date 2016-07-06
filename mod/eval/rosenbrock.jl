dcdf4bca9b3ed098cea6a2879086f90dcc1331e4
Mirko Bunse <mirko.bunse@tu-dortmund.de>
2016-07-06 13:12:32 +0200

    Fixed Rosenbrock experiment

--- mod/eval/rosenbrock.jl
+++ mod/eval/rosenbrock.jl
@@ -7,20 +7,20 @@ import Plotting
 
     Test GD with BT on the Rosenbrock function.
 """
-function rosenbrock_gd_bt(maxiter::Int32=100000) # 1e5
+function rosenbrock_gd_bt(maxiter::Int32=10000) # 1e5
 
     # init rosenbrock function
     rosenbrock = Obj.Objective(
-        w -> 100*(w[2]-w[1]^2)^2 + (1-w[1])^2,
-        function (w)
+        w::Array{Float64,1} -> 100*(w[2]-w[1]^2)^2 + (1-w[1])^2,
+        function (w::Array{Float64,1})
             g2 = 200*(w[2]-w[1]^2)
             return [-2*(w[1]*g2 + (1-w[1])), g2]
         end
     )
 
     # tst
-    w = zeros(2)
-    inf = @time Opt.gd(rosenbrock, w, maxiter=maxiter, printiter=10000)
+    w = randn(2)
+    inf = @time Opt.gd(rosenbrock, w, maxiter=maxiter, printiter=100)
 
     # ask user for plot
     print("\nPlot progress? (y/N): ")
