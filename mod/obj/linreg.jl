0f3c388b581b406db4924cb4d04154f96f6f0a12
mirkobunse <mirko.bunse@cs.tu-dortmund.de>
2016-08-13 16:35:26 +0200

    Super stylish objective function declaration
    Signed-off-by: mirkobunse <mirko.bunse@cs.tu-dortmund.de>

--- mod/obj/linreg.jl
+++ mod/obj/linreg.jl
@@ -1,27 +1,37 @@
 """
     linreg(X, y)
 
-    Objective function and gradient of Linear Regression on data matrix X and label vector y.
-    linreg(X, y).f(w) = vecnorm(y - X*w)^2 / 2
-    linreg(X, y).g(w) = X' * (X*w - y) / length(y)
+    Objective function of Linear Regression on data matrix X and label vector y.
 """
 linreg(X::Array{Float64,2}, y::Array{Float64,1}) = Objective(
-
-    #   f:     w  ->   f(w)
-    w::Array{Float64,1} -> vecnorm(y - X*w)^2 / (2*length(y)),
-
-    #   g:     w  -> ∇ f(w)
-    w::Array{Float64,1} -> vec(X' * (X*w - y)) ./ length(y),
-
-    # f_b: (w, b) ->   f(w) (stochastic with indices b)
-    f_b = (w::Array{Float64,1}, i::Array{Int32,1}) -> vecnorm(y[i] - X[i,:]*w)^2 / (2*length(i)),
-
-    # g_b: (w, b) -> ∇ f(w) (stochastic with indices b)
-    g_b = (w::Array{Float64,1}, i::Array{Int32,1}) -> X[i,:]' * (X[i,:]*w - y[i]) ./ length(i),
-
-    # dim
+    w::Array{Float64,1} -> _linreg_f(X, y, w),  # f
+    w::Array{Float64,1} -> _linreg_g(X, y, w),  # g
+    f_b = (w::Array{Float64,1}, b::Array{Int32,1}) -> _linreg_f(X, y, w, b),
+    g_b = (w::Array{Float64,1}, b::Array{Int32,1}) -> _linreg_g(X, y, w, b),
     dim = length(y)
-    
 )
 
+function _linreg_f(X::Array{Float64,2}, y::Array{Float64,1}, w::Array{Float64,1},
+                   b::Array{Int32,1}=Int32[])
+    if length(b) == 0
+        return vecnorm(y - X*w)^2 / (2*length(y))
+    else
+        return vecnorm(y[b] - X[b,:]*w)^2 / (2*length(b))
+    end
+end
+
+function _linreg_g(X::Array{Float64,2}, y::Array{Float64,1}, w::Array{Float64,1},
+                   b::Array{Int32,1}=Int32[])
+    if length(b) == 0
+        return vec(X' * (X*w - y)) ./ length(y)
+    else
+        return vec(X[b,:]' * (X[b,:]*w - y[b])) ./ length(b)
+    end
+end
+
+"""
+    linreg_predict(w, X)
+
+    Predict the examples in X with Linear Regression under parameter vector w.
+"""
 linreg_predict(w::Array{Float64,1}, X::Array{Float64,2}) = sign(vec(X*w))
--- mod/obj/logreg.jl
+++ mod/obj/logreg.jl
@@ -1,25 +1,37 @@
 """
     logreg(X, y)
 
-    Objective function and gradient of Logistic Regression on data matrix X and label vector y.
+    Objective function of Logistic Regression on data matrix X and label vector y.
 """
 logreg(X::Array{Float64,2}, y::Array{Float64,1}) = Objective(
-
-    #   f:     w  ->   f(w)
-    w::Array{Float64,1} -> sum( log(1 + exp(-y .* X*w)) )[1] / length(y),
-
-    #   g:     w  -> ∇ f(w)
-    w::Array{Float64,1} -> - vec(sum( (y ./ (1 + exp(y .* X*w))) .* X , 1)) ./ length(y),
-
-    # f_b: (w, b) ->   f(w) (stochastic with indices b)
-    f_b = (w::Array{Float64,1}, i::Array{Int32,1}) -> sum( log(1 + exp(-y[i] .* X[i,:]*w)) )[1] / length(i),
-
-    # g_b: (w, b) -> ∇ f(w) (stochastic with indices b)
-    g_b = (w::Array{Float64,1}, i::Array{Int32,1}) -> - vec(sum( (y[i] ./ (1 + exp(y[i] .* X[i,:]*w))) .* X[i,:] , 1)) ./ length(i),
-
-    # dim
+    w::Array{Float64,1} -> _logreg_f(X, y, w),  # f
+    w::Array{Float64,1} -> _logreg_g(X, y, w),  # g
+    f_b = (w::Array{Float64,1}, b::Array{Int32,1}) -> _logreg_f(X, y, w, b),
+    g_b = (w::Array{Float64,1}, b::Array{Int32,1}) -> _logreg_g(X, y, w, b),
     dim = length(y)
-    
 )
 
+function _logreg_f(X::Array{Float64,2}, y::Array{Float64,1}, w::Array{Float64,1},
+                   b::Array{Int32,1}=Int32[])
+    if length(b) == 0
+        return sum( log(1 + exp(-y .* X*w)) )[1] / length(y)
+    else
+        return sum( log(1 + exp(-y[b] .* X[b,:]*w)) )[1] / length(b)
+    end
+end
+
+function _logreg_g(X::Array{Float64,2}, y::Array{Float64,1}, w::Array{Float64,1},
+                   b::Array{Int32,1}=Int32[])
+    if length(b) == 0
+        return - vec(sum( (y ./ (1 + exp(y .* X*w))) .* X , 1)) ./ length(y)
+    else
+        return - vec(sum( (y[b] ./ (1 + exp(y[b] .* X[b,:]*w))) .* X[b,:] , 1)) ./ length(b)
+    end
+end
+
+"""
+    logreg_predict(w, X)
+
+    Predict the examples in X with Logistic Regression under parameter vector w.
+"""
 logreg_predict(w::Array{Float64,1}, X::Array{Float64,2}) = sign(vec(1 ./ (1 + exp(-X*w))) -.5)
