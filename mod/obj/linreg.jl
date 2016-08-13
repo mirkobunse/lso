2800983666ac80ea6119aca317296fbd57bbbe88
mirkobunse <mirko.bunse@cs.tu-dortmund.de>
2016-08-13 16:22:41 +0200

    Nicer comments in objective functions
    Signed-off-by: mirkobunse <mirko.bunse@cs.tu-dortmund.de>

--- mod/obj/linreg.jl
+++ mod/obj/linreg.jl
@@ -6,11 +6,22 @@
     linreg(X, y).g(w) = X' * (X*w - y) / length(y)
 """
 linreg(X::Array{Float64,2}, y::Array{Float64,1}) = Objective(
-    w::Array{Float64,1} -> vecnorm(y - X*w)^2 / (2*length(y)), # f
-    w::Array{Float64,1} -> vec(X' * (X*w - y)) ./ length(y),    # g
+
+    #   f:     w  ->   f(w)
+    w::Array{Float64,1} -> vecnorm(y - X*w)^2 / (2*length(y)),
+
+    #   g:     w  -> ∇ f(w)
+    w::Array{Float64,1} -> vec(X' * (X*w - y)) ./ length(y),
+
+    # f_b: (w, b) ->   f(w) (stochastic with indices b)
     f_b = (w::Array{Float64,1}, i::Array{Int32,1}) -> vecnorm(y[i] - X[i,:]*w)^2 / (2*length(i)),
+
+    # g_b: (w, b) -> ∇ f(w) (stochastic with indices b)
     g_b = (w::Array{Float64,1}, i::Array{Int32,1}) -> X[i,:]' * (X[i,:]*w - y[i]) ./ length(i),
+
+    # dim
     dim = length(y)
+    
 )
 
 linreg_predict(w::Array{Float64,1}, X::Array{Float64,2}) = sign(vec(X*w))
--- mod/obj/logreg.jl
+++ mod/obj/logreg.jl
@@ -4,11 +4,22 @@
     Objective function and gradient of Logistic Regression on data matrix X and label vector y.
 """
 logreg(X::Array{Float64,2}, y::Array{Float64,1}) = Objective(
-    w::Array{Float64,1} -> sum( log(1 + exp(-y .* X*w)) )[1] / length(y),                   # f
-    w::Array{Float64,1} -> - vec(sum( (y ./ (1 + exp(y .* X*w))) .* X , 1)) ./ length(y),   # g
+
+    #   f:     w  ->   f(w)
+    w::Array{Float64,1} -> sum( log(1 + exp(-y .* X*w)) )[1] / length(y),
+
+    #   g:     w  -> ∇ f(w)
+    w::Array{Float64,1} -> - vec(sum( (y ./ (1 + exp(y .* X*w))) .* X , 1)) ./ length(y),
+
+    # f_b: (w, b) ->   f(w) (stochastic with indices b)
     f_b = (w::Array{Float64,1}, i::Array{Int32,1}) -> sum( log(1 + exp(-y[i] .* X[i,:]*w)) )[1] / length(i),
+
+    # g_b: (w, b) -> ∇ f(w) (stochastic with indices b)
     g_b = (w::Array{Float64,1}, i::Array{Int32,1}) -> - vec(sum( (y[i] ./ (1 + exp(y[i] .* X[i,:]*w))) .* X[i,:] , 1)) ./ length(i),
+
+    # dim
     dim = length(y)
+    
 )
 
 logreg_predict(w::Array{Float64,1}, X::Array{Float64,2}) = sign(vec(1 ./ (1 + exp(-X*w))) -.5)
