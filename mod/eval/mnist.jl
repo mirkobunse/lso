d8f7959dd215153f659dce1938e1d45a2fb00d71
mirkobunse <mirko.bunse@cs.tu-dortmund.de>
2016-08-14 11:52:08 +0200

    MNIST eval can be performed on different data sets, writing to different output folders.
    Signed-off-by: mirkobunse <mirko.bunse@cs.tu-dortmund.de>

--- mod/eval/mnist.jl
+++ mod/eval/mnist.jl
@@ -1,6 +1,4 @@
 using Colors
-# using Images
-# using ImageView
 
 import Ls
 import Ls.LineSearch
@@ -8,32 +6,6 @@ import GdOpt
 import GdOpt.GdOptimizer
 import Obj
 import Plotting
-
-# """
-#     mnist_img(row)
-#     
-#     Obtain the row of the MNIST data matrix as image.
-# """
-# function mnist_img(row)
-#     img = Images.grayim(convert( Images.Image{Gray}, reshape(row, (28,28)) ))
-#     img["spatialorder"] = ["y", "x"]
-#     return img
-# end
-# 
-# """
-#     mnist_view(img)
-#     
-#     View an image obtained by mnist_img.
-# """
-# mnist_view(img) = ImageView.view(img) # xy=["y","x"])
-# 
-# """
-#     mnist_saveview(filename, imgview)
-#     
-#     Save the image obtained by mnist_view(example)
-# """
-# mnist_saveimg(filename, img) = Images.save(filename, img)
-
 
 
 """ Quick test function for gradient descent. """
@@ -49,21 +21,20 @@ end
 
 
 """
-    mnist(gdopt, ls, [, seed; ϵ, maxtime, batchsize, assumedgrad])
+    mnist(gdopt, ls, [, folder, seed; ϵ, maxtime, batchsize, assumedgrad])
 
     Evaluate optimizer on MNIST data.
 """
-function mnist(gdopt::GdOptimizer, ls::LineSearch, seed::Int32=1337;
+function mnist(gdopt::GdOptimizer, ls::LineSearch, folder::ASCIIString="seven_vs_all", seed::Int32=1337;
                ϵ::Float64=1e-3, maxtime::Float64=60.0, batchsize::Int32=-1, assumedgrad=false)
 
     srand(seed)
 
-    println("Reading data...")
-    X_train = readdlm("data/seven_vs_all/X_train.dlm")
-    y_train = vec(readdlm("data/seven_vs_all/y_train.dlm"))
-    X_test  = readdlm("data/seven_vs_all/X_test.dlm")
-    y_test  = vec(readdlm("data/seven_vs_all/y_test.dlm"))
-    println("Data consists of $(size(X_train)[1]) training and $(size(X_test)[1]) test examples.")
+    println("Reading data from ./data/$folder...")
+    X_train = readdlm("./data/$folder/X_train.dlm")
+    y_train = vec(readdlm("./data/$folder/y_train.dlm"))
+    X_test  = readdlm("./data/$folder/X_test.dlm")
+    y_test  = vec(readdlm("./data/$folder/y_test.dlm"))
 
     # optimize
     inf, obj, w, acc_train, acc_test, iterrate = _mnist_opt(
@@ -72,7 +43,7 @@ function mnist(gdopt::GdOptimizer, ls::LineSearch, seed::Int32=1337;
     )
 
     # ask user for plot
-    outfile = "./results/seven_vs_all/$(gdopt.name)$((batchsize>0)?batchsize:"")_$(ls.name).pdf"
+    outfile = "./results/$folder/$(gdopt.name)$((batchsize>0)?batchsize:"")_$(ls.name).pdf"
     print("\nDraw to $outfile? (y/N): ")
     if startswith(readline(STDIN), "y")
         println("Plotting...")
