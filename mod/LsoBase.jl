55714c91090a4abbf4e697c11e342e0ea31e1eca
mirkobunse <mirko.bunse@cs.tu-dortmund.de>
2016-08-14 13:43:30 +0200

    Basis for accuracy and metadata DataFrame.
    This will hold info about the classifiers created by optimization.
    
    Signed-off-by: mirkobunse <mirko.bunse@cs.tu-dortmund.de>

--- mod/LsoBase.jl
+++ mod/LsoBase.jl
@@ -19,28 +19,58 @@ end
 
 
 """
-    newinf()
+    new_inf()
 
-    Create new DataFrame to hold iteration info. Will have columns w, opt, iter and lsiter.
+    Create new DataFrame to hold iteration info.
+    Will have columns f, w, opt, iter, lsiter and time.
 """
 new_inf() = DataFrame(
-                w = Array{Float64, 1}[],
-                f = Float64[],
-                opt = Float64[],
-                iter = Int32[],
+                w      = Array{Float64, 1}[],
+                f      = Float64[],
+                opt    = Float64[],
+                iter   = Int32[],
                 lsiter = Int32[],
-                time = Float64[]
+                time   = Float64[]
+            )
+
+"""
+    new_acc()
+
+    Create new DataFrame to hold classifier info.
+    Will have columns gdopt, ls, data, trainsize, trainacc,
+    testacc, time and iterrate.
+"""
+new_acc() = DataFrame(
+                gdopt     = ASCIIString[],
+                ls        = ASCIIString[],
+                data      = ASCIIString[],
+                trainsize = Int32[],
+                trainacc  = Float64[],
+                testacc   = Float64[],
+                time      = Float64[],
+                iterrate  = Float64[]
             )
 
 
 """
-    pushinf!(inf, w, opt, iter, lsiter)
+    push_inf!(inf, w, opt, iter, lsiter)
 
     Will push the given iteration info to DataFrame inf.
 """
 function push_inf!(inf::DataFrame, w::Array{Float64, 1},
                    f::Float64, opt::Float64, iter::Int32, lsiter::Int32, time::Float64=0.0)
     push!(inf, (w, f, opt, iter, lsiter, time))
+end
+
+
+"""
+    push_acc!(acc, gdopt, ls, data, trainsize, trainacc, testacc, time, iterrate)
+
+    Will push the given classifier info to DataFrame acc.
+"""
+function push_acc!(acc::DataFrame, gdopt::ASCIIString, ls::ASCIIString, data::ASCIIString,
+                   trainsize::Int32, trainacc::Float64, testacc::Float64, time::Float64, iterrate::Float64)
+    push!(acc, (gdopt, ls, data, trainsize, trainacc, testacc, time, iterrate))
 end
 
 
